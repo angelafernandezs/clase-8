f1dd4cdcaa209e663b758624e001ff6f8cb2b9da
Angela Fernández <94569441+angelafernandezs@users.noreply.github.com>
2024-10-02 11:30:34 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+[
+    {
+        "nombre": "NUX",
+        "descripcion": "Creación de banca para una persona a partir del principio de tensoestructuras, todo está unido a partir de la tensión de las cuerdas sin ningún tipo de pegamento.",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/angelafernandezs/dno096-08/blob/main/portafolio_010.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Lampara Courvé",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_09.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Ananya",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_03.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Join the Everydaymess|STUSSYXNBHD",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_02.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Láminas y Mostrario",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_05.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Editorial inspirada en Vogue",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_06.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Afiche Canción del Niño y el Ciervo",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_01.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Afiche No sé que Decirte",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_08.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Sensaciones",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_04.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Cerámicas",
+        "fecha": "2024",
+        "signo": "Artesanía",
+        "foto": "https://github.com/Franirribarrat/dno096-08/blob/main/portafolio_07.png?raw=true",
+        "github": "https://github.com/angelafernandezs"
+    }
+]
+
